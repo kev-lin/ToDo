287822f65e78ac56f6fa42e2864e666f44ef8d37
Kevin Lin <kzhlin@gmail.com>
2014-11-02 17:28:34 -0800

    I am a Pokemon master.

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,8 +1,42 @@
 class PokemonsController < ApplicationController
+
+	def new
+		render 'new'
+	end
+
+	def create
+		@pokemon = Pokemon.new(pokemon_params)
+		@pokemon.trainer = current_trainer
+		@pokemon.health = 100
+		@pokemon.level = 1
+		if @pokemon.save
+			redirect_to @pokemon.trainer
+		else
+			flash[:error] = @pokemon.errors.full_messages.to_sentence
+			redirect_to new_path
+		end
+	end
+
 	def capture
     @pokemon = Pokemon.find(params[:id])
-		@pokemon.trainer_id = current_trainer
+		@pokemon.trainer = current_trainer
 		@pokemon.save
 		redirect_to root_path
+	end
+
+	def damage
+		@pokemon = Pokemon.find(params[:id])
+		@trainer = @pokemon.trainer_id
+		@pokemon.health = @pokemon.health - 10
+		@pokemon.save
+		if @pokemon.health <= 0
+			@pokemon.destroy
+		end
+		redirect_to trainer_path(@trainer)
+	end
+
+	private
+	def pokemon_params
+		params.require(:pokemon).permit(:name)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :trainers
   resources :trainers
   patch '/pokemons/capture', to: 'pokemons#capture', as: 'capture'
+  patch '/pokemons/damage', to: 'pokemons#damage', as: 'damage'
+  post '/pokemons/create', to: 'pokemons#create', as: 'create'
+  get '/pokemons/new', to: 'pokemons#new', as: 'new'
 end
