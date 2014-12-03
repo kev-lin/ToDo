b08d76b619119e17c0df71f902a4d79524db3148
Victor <victorhlu13@gmail.com>
2014-12-03 00:38:53 -0800

    Changes to task functionality

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -7,8 +7,6 @@ class PokemonsController < ApplicationController
 	def create
 		@pokemon = Pokemon.new(pokemon_params)
 		@pokemon.trainer = current_trainer
-		@pokemon.health = 100
-		@pokemon.level = 1
 		if @pokemon.save
 			redirect_to @pokemon.trainer
 		else
@@ -27,11 +25,7 @@ class PokemonsController < ApplicationController
 	def damage
 		@pokemon = Pokemon.find(params[:id])
 		@trainer = @pokemon.trainer_id
-		@pokemon.health = @pokemon.health - 10
-		@pokemon.save
-		if @pokemon.health <= 0
-			@pokemon.destroy
-		end
+		@pokemon.destroy
 		redirect_to trainer_path(@trainer)
 	end
 
