e4617fc75d21aee79aba34c2e4e9e44c0fd4748c
Victor <victorhlu13@gmail.com>
2014-12-05 11:49:43 -0800

    changes to routes/other minor quality changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
-  patch '/pokemons/capture', to: 'pokemons#capture', as: 'capture'
-  patch '/pokemons/damage', to: 'pokemons#damage', as: 'damage'
-  post '/pokemons/create', to: 'pokemons#create', as: 'create'
-  get '/pokemons/new', to: 'pokemons#new', as: 'new'
+  patch '/user/task', to: 'pokemons#capture', as: 'capture'
+  patch '/user/delete', to: 'pokemons#damage', as: 'damage'
+  patch '/user/destroyer', to: 'pokemons#destroyer', as: 'destroyer'
+  post '/user/create', to: 'pokemons#create', as: 'create'
+  get '/user/new', to: 'pokemons#new', as: 'new'
 end
